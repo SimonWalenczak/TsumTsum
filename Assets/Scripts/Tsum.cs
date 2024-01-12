f3b537e5d940cc0c3cbc3737782b3486db3785b2
Simon Walenczak <92367148+SimonWalenczak@users.noreply.github.com>
2024-01-12 12:02:05 +0100

    azertyu

--- Assets/Scripts/Tsum.cs
+++ Assets/Scripts/Tsum.cs
@@ -1,7 +1,27 @@
+using System;
 using UnityEngine;
 
 public class Tsum : MonoBehaviour
 {
     public int IndexType;
     public bool CanBeSelected;
+
+    private void OnMouseDrag()
+    {
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, 0.8f);
+
+        foreach (var collider in collider2DArray)
+        {
+            if (collider.gameObject.GetComponent<Tsum>().IndexType == IndexType)
+            {
+                collider.gameObject.GetComponent<Tsum>().CanBeSelected = true;
+            }
+        }
+    }
+    // private void O
+    // {
+    //     
+    //     
+    //     Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, 0.8f);
+    // }
 }
